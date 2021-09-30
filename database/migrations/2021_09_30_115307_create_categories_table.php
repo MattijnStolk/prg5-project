2c5ce7bdefc4e2afc35e1934410cd62df473c2ae
Mattijn <0986087@hr.nl>
2021-09-30 15:10:19 +0200

    worked on migrations

---
+++ database/migrations/2021_09_30_115307_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->boolean('work');
+            $table->boolean('progress');
+            $table->boolean('past-projects');
+            $table->boolean('school');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
