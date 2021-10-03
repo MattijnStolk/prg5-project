4dc0203ad94a0e83074d1c1ac28cc48e99293875
MattijnStolk <mattijn001@gmail.com>
2021-10-03 19:49:00 +0200

    added login

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/about', [AboutController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
