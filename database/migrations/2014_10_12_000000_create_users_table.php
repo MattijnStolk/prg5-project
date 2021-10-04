aa75f3963293b45d3b8cecf131d80cfac4bd7055
Mattijn <0986087@hr.nl>
2021-10-04 14:22:23 +0200

    added login and register page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('edited_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
             $table->rememberToken();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/about', [AboutController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
